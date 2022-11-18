39dcd2928b4b4453dd12dc182e106f773906db8a
MilosStankovic6690 <Milosstankovic6690@gmail.com>
2022-11-18 20:00:22 +0100

    Add project files.

---
+++ SeleniumPOM/Page/HomePage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using WebDriver = SeleniumPOM_SauceDemo.Driver.WebDriver;
+
+namespace SeleniumPOM_SauceDemo.Page;
+
+public class HomePage
+{
+    private IWebDriver driver = WebDriver.Instance;
+
+    public IWebElement Backpack => driver.FindElement(By.CssSelector(".inventory_item_img a"));
+    public IWebElement AddToCardButon => driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
+    public IWebElement BackButton => driver.FindElement(By.Id("back-to-products"));
+
+   
+
+    
+
+}
---
+++ SeleniumPOM/TetsClass.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+using SeleniumPOM_SauceDemo.Driver;
+using SeleniumPOM_SauceDemo.Page;
+
+namespace SeleniumPOM_Saucedemo;
+
+public class TestClass
+{
+    private LoginPage _loginPage;
+    private HomePage _homePage;
+
+    [SetUp]
+    public void BeforeScenario()
+    {
+        WebDriver.Initialize();
+        _loginPage = new LoginPage();
+        _homePage = new HomePage();
+    }
+
+    [TearDown]
+    public void AfterScenario()
+    {
+        WebDriver.CleanUp();
+    }
+
+
+    [Test]
+
+    public void TC01_FillUsernameAndPasswordWithValidData_ShouldInventoryPageDisplayed()
+    {
+        Login("standard_user", "secret_sauce");
+        
+    }
+
+    [Test]
+    public void TC02_ClickOnBackpack_ShouldDisplayedBackpackDetail()
+    {
+        Login("standard_user", "secret_sauce");
+        _homePage.Backpack.Click();
+    }
+
+
+    [Test]
+    public void TC03_ClickButtonAddToCart_ProductShouldBeAdd()
+    {
+        Login("standard_user", "secret_sauce");
+        _homePage.Backpack.Click();
+        _homePage.AddToCardButon.Click();
+    }
+
+    [Test]
+    public void TC04_ClickOnBackButton_InventoryPageShouldBeDisplayed()
+    {
+        Login("standard_user", "secret_sauce");
+        _homePage.Backpack.Click();
+        _homePage.AddToCardButon.Click();
+        _homePage.BackButton.Click();
+    }
+
+
+    public void Login(string name, string password)
+    {
+        _loginPage.LoginOnPage(name, password);
+    }
+}
